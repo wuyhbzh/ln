aee996a10b5913d3c40dd92faf4dfb2620c94dc1
wuyhbzh <wuyhbzh@163.com>
2016-12-12 22:58:01 +0800

    fix

--- py/task_new/build.bat
+++
@@ -1,9 +0,0 @@
-:: 可配参数
-:: -dir AVI	游戏文件夹名称=AVI
-:: -id 901  游戏id=901
-:: -num 6 	最大玩家数量=6
-:: -f   	删除文件夹时询问
-:: -u		只更新配置，不新建文件夹
-::python build.py -dir AVGouji -id 901 -num 6
-python build.py
-pause
--- py/task_new/publish.bat
+++
@@ -1,5 +0,0 @@
-:: 可配参数 
-:: -dir 文件夹名称
-:: python publish.py -dir AVGouji
-python publish.py
-pause
