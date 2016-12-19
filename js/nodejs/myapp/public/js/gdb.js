ed55535162540e4dabbf827fcff4254490b2e07c
wuyhbzh <wuyhbzh@163.com>
2016-12-19 23:12:23 +0800

    nodejs sequelize db

--- js/nodejs/myapp/public/js/gdb.js
+++ js/nodejs/myapp/public/js/gdb.js
@@ -4,32 +4,67 @@ var sequelize = new Sequelize('gamebzh', 'gamebzh', '2889372', {host : '115.159.
 var prIsLoaded = false;
 
 // 定义表， sync的时如没有此表将创建表
-var Users = sequelize.define('tusers',{
+var Users = sequelize.define('users',{
 	id : {type : Sequelize.INTEGER, autoIncrement : true, primaryKey : true, unique : true},
 
 	name : {type : Sequelize.STRING, unique : true},
 
 	pass : {type : Sequelize.STRING},
 
-	phoneNum : {type : Sequelize.STRING},
+	email : {type : Sequelize.STRING},
 
 });
 
-console.log('load gdb')
+console.log('load gdb................')
 
-// exports.getInstance = function(){
-// 	if (prIsLoaded) {
-// 		console.log('return gdb')
-// 		return exports;
-// 	}else{
-// 		prIsLoaded = true;
-// 		//sequelize.sync({force:false});
-// 		console.log('sync gdb')
-// 	}
-// }
+sequelize.sync({force:false}).then(function(){
+	console.log('sync success');
+});
 
-exports.getUsers = function(cb){
-	Users.findOne({
-		where:{id:1}
-	}).then(cb);
+
+exports.addUser = function(param, callback) {
+	Users.create({
+		name:  	param.name,
+		pass: 	param.pass,
+		email:  param.email || '',
+	}).then(function(result){
+		console.log('addUser ok: ' + JSON.stringify(result) );
+		callback(result);
+	}).catch(function(err){
+		console.log('addUser failed: ' + err);
+		callback(false);
+	});
+}
+
+exports.findUser = function(param, callback){
+    Users.findOne({
+        where: {
+            name: 	param.name,
+            pass: 	param.pass,
+            email: 	param.email || '',
+        }
+    }).then(function(user){
+    	// console.log(`find ${user.length} user:`);
+        console.log(JSON.stringify(user));
+        callback(user);
+    }).catch(function(err){
+    	console.log('findUser failed: ' + err);
+    	callback(err);
+    });
+}
+
+exports.showAll = function(callback){
+	Users.findAll().then(function(users){
+		console.log(`find ${users.length} users:`);
+	    for (let user of users) {
+	    	if (user){
+	        	console.log(JSON.stringify(user));
+	    	}
+	    }
+	    callback(users);
+
+	}).catch(function(err){
+		console.log('showAll failed: ' + err);
+		callback(false);
+	})
 }
--- js/nodejs/myapp/routes/users.js
+++ js/nodejs/myapp/routes/users.js
@@ -1,25 +1,71 @@
 var express = require('express');
-// var gdb = require('../public/js/gdb')
-// var mdb = require('../public/js/mdb')
+var url = require('url');
 var router = express.Router();
+var gdb = require('../public/js/gdb')
+// var mdb = require('../public/js/mdb')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
 
-	res.send('users get ' + req.url);
-	console.log('users get ');
+	// res.send('users get ' + req.url);
+	console.log('users get');
 
 	// gdb.getUsers(function(data){
 	// 	console.log(data.name);
 	// 	res.send('get ' + data.name);
 	// });
+	next();
 
 });
 
-router.get('/:name?', function(req, res, next){
-	console.log('users get baba ' + req.params.name);
-	res.send('users get baba ' + req.url);
+router.get('/', function(req, res, next) {
+
+	res.send('showall sign logic');
+	console.log('users get 1');
+
+});
+
+
+router.get('/sign', function(req, res, next){
+	var urlJson= url.parse(req.url, true);
+	var param = urlJson.query;
+	console.log(param);
+
+	gdb.findUser(param, function(data){
+		if (data) {
+			// console.log('sign user success');
+			res.send('sign user success')
+		}else{
+			// console.log('sign user faile');
+			res.send('sign user faile');
+		}
+	});
 
 })
+
+router.get('/logic', function(req, res, next) {
+	var urlJson= url.parse(req.url, true);
+	var param = urlJson.query;
+	console.log(param);
+	console.log('logic ' + param.name + param.pass);
+
+	gdb.addUser(param, function(data){
+		if (data) {
+			res.send('findall user success');
+		}else{
+			res.send('can not findall user');
+		}
+	});
+});
+
+router.get('/showall', function(req, res, next){
+	gdb.showAll(function(data){
+		if(data){
+			res.send(data);
+		}else{
+			res.send('showall faile');
+		}
+	});
+});
 
 module.exports = router;
