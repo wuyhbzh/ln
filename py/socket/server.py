af48838a99dc03eb0dbc056d46606f5992e68d7a
wuyhbzh <wuyhbzh@163.com>
2016-12-26 23:20:22 +0800

    py socket

--- py/socket/server.py
+++ py/socket/server.py
@@ -1,35 +1,43 @@
+#coding=utf-8
 import socket
 import time
 import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-s.bind(('127.0.0.1', 9999))
+host = '127.0.0.1'
+port = 9999
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+print('socket build')
+
+s.bind((host, port))
+print('socket bind')
 
 s.listen(5)
-
-print('Waiting for connection...')
+print('socket listen')
 
 def tcplink(sock, addr):
-	print('Accept new connection from 1 %s:%s...' % addr)
-	sock.send(b'Welcom!')
-	print('Accept new connection from 2 %s:%s...' % addr)
+	print('connection from %s:%s' % addr)
+
+	# 发送二进制数据到 addr
+	sock.send(b'welcome! exit to close')
+	print('send Welcome to %s:%s' % addr)
 
 	while True:
-		print("wait recv 1")
 		data = sock.recv(1024)
-		print("wait recv 2")
+		print(data)
 		time.sleep(1)
 		if not data or data.decode('utf-8') == 'exit':
 			break
-		sock.send(('Hello, %s!' % data.decode('utf-8')).encode('utf-8'))
+		sock.send(data)
+
 	sock.close()
-	print('Connection from %s:%s closed.' % addr)
+	print('connection from %s:%s closed.' % addr)
 
 while True:
-	print("run 1")
+	print("\nsocket accept begin...")
 	sock, addr = s.accept()
-	print("run 2")
+	print("socket accept something")
 	t = threading.Thread(target=tcplink, args=(sock, addr))
 	t.start()
 
