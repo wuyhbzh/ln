32083e80b5d30c9888c293972ca1d6be7554c503
wuyhbzh <wuyhbzh@163.com>
2017-06-09 15:29:18 +0800

    estools

--- js/nodejs/main.js
+++ js/nodejs/main.js
@@ -7,51 +7,14 @@ var fs = require('fs');
 var changeVar = function(pData, pVarName){
     // pData = fs.readFileSync("./js/nodejs/tsetast.js").toString();
     var ast = esprima.parse(pData);
-
     ast = esmangle.mangle(ast);
-
-    var varlist = [];
-    var varindex = 1;
-
-    // estraverse.traverse(ast,{
-    //     enter: function(node){
-    //         // console.log(node);
-    //         // console.log("");
-    //         varlist = [];
-    //         varindex = 1;
-    //         if (node.body && node.body.forEach){
-    //             node.body.forEach(function(value, index, array){
-    //                 if (value.kind && value.kind === 'var' && value.type == 'VariableDeclaration') {
-    //                     for (var key in value.declarations) {
-    //                         varlist[value.declarations[key].id.name] = varindex;
-    //                         varindex = varindex + 1;
-    //                     }
-    //                 }
-    //             });
-    //         }
-    //     },
-
-    //     leave: function(node){
-    //         if (node.type === "Identifier" && varlist[node.name]) {
-    //             node.name = "a" + varlist[node.name];
-    //         }
-    //     },
-    // });
-
-    // estraverse.traverse(ast,{
-    //     enter: function(node){
-    //         if (node.type === "Identifier" && varlist[node.name]){
-    //             node.name = "a" + varlist[node.name];
-    //         }
-    //     },
-    // });
-
     return escodegen.generate(ast);
 }
 
 
 function walk(path, floor, handleFile) {
     handleFile(path, floor);
+    console.log("path:" + path);
     floor++;
     fs.readdir(path, function (err, files) {
         if (err) {
@@ -86,14 +49,19 @@ function handleFile(path, floor, fileName) {
 
         } else if (stats.isDirectory()) {
         } else  {
-            fs.readFile(tmpPath, function (err, data) {
-                data = data.toString();
-                data = changeVar(data, fileName);
-                fs.writeFile(tmpPath, data);
-                console.log("write " + tmpPath);
-            });
+            console.log("write " + tmpPath);
+            
+            // fs.readFile(tmpPath, function (err, data) {
+            //     data = data.toString();
+            //     data = changeVar(data, fileName);
+            //     fs.writeFile(tmpPath, data);
+            // });
+
+            var data = fs.readFileSync(tmpPath).toString();
+            data = changeVar(data, fileName);
+            fs.writeFile(tmpPath, data);
         }
     })
 } 
 
-walk("C:/Users/mx/Desktop/sdk",  0,  handleFile);
+walk("D:/work/client/trunk-cr/src",  0,  handleFile);
