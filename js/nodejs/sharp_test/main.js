e93a84719d215893f370eb7384a6adfa6d316a59
wuyhbzh <wuyhbzh@163.com>
2017-07-06 03:37:34 +0800

    实现切割

--- js/nodejs/sharp_test/main.js
+++ js/nodejs/sharp_test/main.js
@@ -6,10 +6,30 @@ var res = rootpath + "dingque.png";
 
 var sharp = require('sharp');
 
-sharp(res).resize(300, 200).toFile(rootpath + 'output1.jpg', function(err) {
-  if (err) {
-    throw err;
-  }
-  // output.jpg is a 300 pixels wide and 200 pixels high image
-  // containing a scaled and cropped version of input.jpg
+// sharp(res)
+//   .extract({left:2, top:2, width:77, height:79})
+//   .toFile(rootpath + "new.png", function(err){});
+
+// var jsonstr = fs.readFileSync(rootpath + "dingque.plist");
+// var xmldoc = new XMLDocument();
+// xmldoc.lo
+// var jsonObj = jsonstr.toJSON();
+// console.log(jsonObj);  
+
+var parseString = require('xml2js').parseString;
+var xml = fs.readFileSync(rootpath + "dingque.plist");
+parseString(xml, function (err, result) {
+    console.log(result);
+
+    var pnglist = result.plist.dict[0].dict[0].dict;
+    
+    for (var key in pnglist){
+      var numstr = pnglist[key].string[0];
+      var numarr = numstr.match(/\d+(\.\d+)?/g);
+      console.log(numarr);
+      
+      sharp(res)
+        .extract({left:parseInt(numarr[0]), top:parseInt(numarr[1]), width:parseInt(numarr[2]), height:parseInt(numarr[3]) } )
+        .toFile(rootpath + key + "new.png", function(err){});
+    }
 });
