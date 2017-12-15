34f0b9cd8db6176b7fe5f5973ca9ca60c4598e02
wuyhbzh <wuyhbzh@163.com>
2017-12-15 10:24:29 +0800

    pixijs-t

---
+++ js/pixit/src/entry.js.js
@@ -0,0 +1,4 @@
+var m = require('./module.js');
+var m2 = require('./module2.js');
+var m2 = require('./main.js');
+console.log(m, m2);
---
+++ js/pixit/src/main.js
@@ -0,0 +1,34 @@
+// https://www.codeandweb.com/physicseditor
+let Application = PIXI.Application,
+    Container = PIXI.Container,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    TextureCache = PIXI.utils.TextureCache,
+    Sprite = PIXI.Sprite;
+
+let app = new Application({
+    width: 256,
+    height: 256,
+    antialiasing: true,
+    transparent: false,
+    resolution: 1
+});
+
+document.body.appendChild(app.view);
+
+loader
+    .add("assert/blob.png")
+    .load(setup);
+
+let cat;
+
+function setup() {
+    cat = new Sprite(resources["assert/blob.png"].texture);
+    cat.y = 96;
+    app.stage.addChild(cat);
+    app.ticker.add(delta => gameLoop(delta));
+}
+
+function gameLoop(delta) {
+    cat.x += 1;
+}
